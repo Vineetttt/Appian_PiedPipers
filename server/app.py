57f56f3ecf1673b01d3a7292548bb72517ec1e74
Vineetttt <vineetchotaliya30@gmail.com>
2023-12-20 17:34:08 +0530

    adding sql query generation endpoint

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from query_generation.generate_sql_using_palm import generate_sql
 from flask_cors import CORS
 
 from image_to_code.code_generation import generate_code_from_caption
@@ -11,7 +12,24 @@ UPLOAD_FOLDER = 'static/uploads'
 @app.route('/api/v1/')
 def index():
     return jsonify({'message': 'Welcome to the Image Captioning and Code Generation API'})
-    
+
+@app.route('/api/v1/query_generation/using_palm', methods=['POST'])
+def generate_sql_using_palm():
+    try:
+        data = request.get_json()
+
+        table_name = data.get("table_name")
+        columns = data.get("columns")
+        query_input = data.get("query_input")
+
+        result = generate_sql(table_name, columns, query_input)
+
+        return jsonify({"sql_query": result})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
 @app.route('/api/v1/code_generation/from_image', methods=['POST'])
 def generate_code_from_image():
     try:
